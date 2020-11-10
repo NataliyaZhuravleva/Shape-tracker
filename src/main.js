a5e2c8ee9e81bf4e56e70a604c5c15426823ddf6
Nataliya Zhuravleva <natalindria@gmail.com>
2020-11-10 00:55:57 -0800

    full testing of Triangle with Jest

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import $ from 'jquery';
 import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import '../../../useful/_New Project Template/src/node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import Triangle from './triangle.js';
 
